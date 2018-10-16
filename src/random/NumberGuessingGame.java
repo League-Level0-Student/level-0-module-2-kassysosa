e430e90d1bb1ff38ef76445be2cf342ceb7a55ce
350969 <350969@0338S-S-R0B6ZH2.sdcs.local>
2018-10-16 14:24:53 -0700

    NumberGuessngGame(Complete)

--- src/random/NumberGuessingGame.java
+++ src/random/NumberGuessingGame.java
@@ -7,20 +7,22 @@ import javax.swing.JOptionPane;
 public class NumberGuessingGame {
 public static void main(String[] args) {
 	
-    	String answer = JOptionPane.showInputDialog("Guess a number between 0, 1, and 2");
-	
+	String answer = JOptionPane.showInputDialog("Guess a number between 0, 1, and 2");
 	int r = 0; 
     Random factory = new Random(); 
     r = factory.nextInt(2);
     System.out.println(r);
-    if(answer.equals(r)) {
+   
+    int num = Integer.parseInt(answer);
+    if(num == r) {
     	JOptionPane.showMessageDialog(null, "Correct!");
+    }
+   
     
-    
+    else {	JOptionPane.showMessageDialog(null, "Better luck next time!");
+    	
     }
-    else {
-    	JOptionPane.showMessageDialog(null, "Better luck next time!");
-    }
-    }
-}
+    }}
+
+
 
