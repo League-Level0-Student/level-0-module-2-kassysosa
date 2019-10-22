3e0941d112eac1b384b562a15dd464ce3f021bc7
350969 <350969@0338S-Z-90TDE0Q.sdcs.local>
2019-10-22 14:13:19 -0700

    ObidientRobot(i.p.)

--- src/ObidientRobot.java
+++ src/ObidientRobot.java
@@ -1,5 +1,42 @@
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class ObidientRobot {
+	 static Robot purple = new Robot();
 public static void main(String[] args) {
-	
+	drawSquare();
+	drawTriangle();
+	drawCircle();
 }
+public static void drawSquare() {
+	purple.penDown();
+	purple.setSpeed(20);
+	purple.move(300);
+	purple.turn(90);
+	purple.move(300);
+	purple.turn(90);
+	purple.move(300);
+	purple.turn(90);
+	purple.move(300);
+}
+public static void drawTriangle() {
+	purple.penDown();
+	purple.setSpeed(20);
+	purple.setPenColor(Color.yellow);
+	purple.move(100);
+	purple.turn(150);
+	purple.move(300);
+	purple.turn(90);
+	purple.move(180);
+	purple.turn(120);
+	purple.move(210);
+}
+public static void drawCircle() {
+	for(int i = 0; i < 360; i++) {
+		purple.move(1);
+		purple.turn(360/360);
+	}
+}
+
 }
