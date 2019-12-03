905734c2d0747953eafc9565a46cc33101d503fb
350969 <350969@0338S-Z-90TDE0Q.sdcs.local>
2019-12-03 14:23:37 -0800

    Houses2(pointyhouses), Houses(ip)

--- src/Houses.java
+++ src/Houses.java
@@ -5,30 +5,36 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	public static void main(String[] args) {
-		Robot purple = new Robot();
-		purple.moveTo(10, 550);
+	Robot purple = new Robot();
+		purple.moveTo(10,550);	
+		purple.setSpeed(50);
+		purple.penDown();
 		Robot.setWindowColor(Color.black);
 		Random h = new Random();
 		
 		for (int i = 0; i < 10; i++) {
-	int height = h.nextInt(401)+100;
-			purple.setRandomPenColor();
 		
-		purple.penDown();
-		purple.setSpeed(40);
-		purple.move(height);
-		purple.turn(90);
-		purple.move(70);
-		purple.turn(90);
-		purple.move(height);
-		purple.turn(270);
-		purple.move(20);
-		purple.turn(270);
+			int height = h.nextInt(401)+100;
+			if( height >= 30) {
+			purple.setRandomPenColor();
+			purple.move(height);
+			purple.turn(90);
+			purple.move(70);
+			purple.turn(90);
+			purple.move(height);
+			purple.turn(270);
+			purple.move(20);
+			purple.turn(270);
 
-if(height < 30) {
-	
-}
+			
+		}
+			else if{
+				purple.move(height);
+				purple.turn(45);
+			
+			}
+		
 		
 		}
+		}
 	}
-}
---
+++ src/Houses2.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+	import java.util.Random;
+
+	import org.jointheleague.graphical.robot.Robot;
+
+	public class Houses2 {
+		public static void main(String[] args) {
+			Robot purple = new Robot();
+			purple.moveTo(10, 550);
+		
+			
+			
+			
+		
+				purple.penDown();
+				purple.setSpeed(50);
+				purple.move(100);
+				purple.turn(45);
+				purple.move(50);
+				
+					
+		
+
+		}
+			
+		}
+	
+
+
